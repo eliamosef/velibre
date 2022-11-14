6bbe6fc6601fbdd20bad18f1b90b19e4abad51b8
root <root@localhost.localdomain>
2022-11-15 00:51:44 +0100

    Processing data

---
+++ data-processer/src/main/python/process.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sys
+
+
+input_file=sys.argv[1]
+city=sys.argv[2]
+print('You chose : ' + input_file)
+print('You chose : ' + city)
+
+availability_df=pd.read_csv(input_file, sep = ";")
+count=availability_df[(availability_df.is_installed == "OUI") & (availability_df.nom_arrondissement_communes == city)]["numbikesavailable"].sum()
+print("The number of available bikes in: " + city + " is: " + str(count)) 
